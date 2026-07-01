9c5b39200d9cedb39bc7fb23b6bff7ad6a06e051
boda <6238558+bodadotsh@users.noreply.github.com>
2026-07-01 17:21:23 +0100

    Amphetamine

--- modules/darwin/home-manager.nix
+++ modules/darwin/home-manager.nix
@@ -38,7 +38,8 @@ in
     # you may receive an error message "Redownload Unavailable with This Apple ID".
     # This message is safe to ignore. (https://github.com/dustinlyons/nixos-config/issues/83)
     masApps = {
-      "uBlock Origin Lite" = 6745342698;  
+      "uBlock Origin Lite" = 6745342698;
+      "Amphetamine" = 937984704;
     };
   };
 
